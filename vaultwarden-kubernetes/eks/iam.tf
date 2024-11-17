e0d2e6e563a0fea815bf0ca72f6e22221bb0de71
Lester Guerzon <guerzon@proton.me>
2024-11-17 18:41:50 +0800

    feat: EKS addons rework (stash)

--- vaultwarden-kubernetes/eks/iam.tf
+++ vaultwarden-kubernetes/eks/iam.tf
@@ -1,3 +1,18 @@
+data "tls_certificate" "this" {
+  count = var.enable_irsa ? 1 : 0
+  url   = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "this" {
+  count = var.enable_irsa ? 1 : 0
+
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.this[0].certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+##### Cluster permissions #####
+
 resource "aws_iam_role" "cluster" {
   name               = "VaultwardenEKSClusterIAMRole-${var.environment}"
   assume_role_policy = <<POLICY
@@ -34,6 +49,7 @@ resource "aws_iam_role" "nodes" {
     POLICY
 }
 
+## Allows EKS to create EC2 instances and Load-balancers
 resource "aws_iam_role_policy_attachment" "cluster" {
   role       = aws_iam_role.cluster.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
