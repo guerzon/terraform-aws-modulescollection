1d4dc33ea60488defc14353aeedfce595f9b8f68
Lester Guerzon <guerzon@proton.me>
2024-11-30 22:27:17 +0800

    fix: Resource depends on aws_eks_node_group (#7)

--- vaultwarden-kubernetes/eks/ebscsidriver.tf
+++ vaultwarden-kubernetes/eks/ebscsidriver.tf
@@ -35,6 +35,9 @@ resource "aws_eks_addon" "ebs_csi_driver" {
   addon_name               = "aws-ebs-csi-driver"
   service_account_role_arn = aws_iam_role.ebs_csi.arn
   preserve                 = false
+  depends_on = [
+    aws_eks_node_group.this
+  ]
 }
 
 resource "kubernetes_storage_class" "ebs_csi_sc" {
